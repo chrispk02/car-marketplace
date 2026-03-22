f629406354bce4ec031315a5653f4c16da384f61
chrispk02 <christran.pk02@gmail.com>
2026-03-23 01:11:41 +0700

    Bypass typescript errors for deployment

--- Frontend/postcss.config.mjs
+++ Frontend/postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // Thêm @ vào trước và /postcss vào sau
+    '@tailwindcss/postcss': {}, // Thêm @tailwindcss/postcss vào đây
   },
 };
 
