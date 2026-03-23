87afa2b62238e1ce20789cef0392b2e8dfd89923
anhtran3 <anhtran3@tdt.asia>
2026-03-23 15:32:12 +0700

    fix: add vercel config

--- Frontend/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // Thêm @tailwindcss/postcss vào đây
-  },
-};
-
-export default config;
